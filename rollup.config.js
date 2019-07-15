e79906f70d0fafc7bdfa3b83a3ec1f2aed8baa09
Vanilagy <dpayr@musica-viva.de>
2019-07-15 18:28:01 +0200

    Add TS stuffz

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+    input: './client/ts/index.ts',
+    plugins: [
+        typescript({
+            abortOnError: false
+        })
+    ],
+    output: {
+        format: 'iife',
+        file: './client/js/bundle.js',
+        name: '', // Empty string here to create an unnamed IIFE
+    }
+}
