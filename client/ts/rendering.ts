91ded4f7387bc2a620abca150fecb5279368ff6c
finbargiusti <finbargiusti@gmail.com>
2019-07-15 18:14:00 +0100

    Add basic scene

--- client/ts/rendering.ts
+++ client/ts/rendering.ts
@@ -1,3 +1,30 @@
 import * as THREE from "three";
 
-// things
+// CONFIG
+
+const FOV = 75;
+
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(
+  FOV,
+  window.innerWidth / window.innerHeight
+);
+
+let renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+let animate = () => {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+};
+animate();
