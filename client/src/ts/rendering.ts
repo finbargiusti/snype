98dc805cb44d058f95effb9b3c2a4b5be67b5396
Vanilagy <dpayr@musica-viva.de>
2019-07-15 22:43:51 +0200

    Auto stash before merge of "dev" and "origin/dev"

--- client/src/ts/rendering.ts
+++ client/src/ts/rendering.ts
@@ -10,11 +10,13 @@ import {
 
 class Player {
   public position: THREE.Vector3;
+  public velocity: THREE.Vector3;
   public yaw: number = 0;
   public pitch: number = 0;
 
   constructor(x: number, y: number, z: number) {
     this.position = new THREE.Vector3(x, y, z);
+    this.velocity = new THREE.Vector3(0, 0, 0);
   }
 
   getHeadPosition() {
@@ -51,31 +53,23 @@ window.addEventListener("keydown", e => {
   console.log(keyCode);
 
   switch (keyCode) {
-    case 87:
-      {
-        inputState.forwards = true;
+    case 87: {
+      inputState.forwards = true;
+    }; break;
+    case 83: {
+      inputState.backwards = true;
+    }; break;
+    case 65: {
+      inputState.left = true;
+    }; break;
+    case 68: {
+      inputState.right = true;
+    }; break;
+    case 32: {
+      if (localPlayer.position.z === 0) {
+        localPlayer.velocity.z = 5;
       }
-      break;
-    case 83:
-      {
-        inputState.backwards = true;
-      }
-      break;
-    case 65:
-      {
-        inputState.left = true;
-      }
-      break;
-    case 68:
-      {
-        inputState.right = true;
-      }
-      break;
-    case 37:
-      {
-        camera.rotateY(0.03);
-      }
-      break;
+    }; break;
   }
 });
 
@@ -153,7 +147,9 @@ renderer.domElement.addEventListener("click", () => {
   renderer.domElement.requestPointerLock();
 });
 
-renderer.domElement.addEventListener("mousemove", e => {
+renderer.domElement.addEventListener('mousemove', (e) => {
+  if (pointerLocked === false) return;
+
   let x = e.movementX;
   let y = e.movementY;
 
@@ -179,6 +175,9 @@ function lockChangeAlert() {
     //document.removeEventListener("mousemove", updatePosition, false);
   }
 }
+
+
+let grav = new THREE.Vector3(0, 0, -10);
 
 const effectPass = new EffectPass(
   camera,
@@ -216,38 +215,36 @@ let animate = () => {
   camera.rotateOnWorldAxis(xAxis, localPlayer.pitch);
   camera.rotateOnWorldAxis(zAxis, localPlayer.yaw);
 
-  var lookAtVector = new THREE.Vector3(0, 0, -1);
+  /*var lookAtVector = new THREE.Vector3(0, 0, -1);
   lookAtVector.applyQuaternion(camera.quaternion);
 
   lookAtVector.setZ(0);
-  lookAtVector.normalize();
+  lookAtVector.normalize();*/
 
+  let movementVec = new THREE.Vector3(0, 0, 0);
   if (inputState.forwards) {
-    localPlayer.position.add(
-      lookAtVector.multiplyScalar(playerSpeed * (dif / 1000))
-    );
+    movementVec.y += 1;
   }
   if (inputState.backwards) {
-    lookAtVector.applyAxisAngle(zAxis, Math.PI);
-
-    localPlayer.position.add(
-      lookAtVector.multiplyScalar(playerSpeed * (dif / 1000))
-    );
+    movementVec.y -= 1;
   }
   if (inputState.left) {
-    lookAtVector.applyAxisAngle(zAxis, Math.PI / 2);
-
-    localPlayer.position.add(
-      lookAtVector.multiplyScalar(playerSpeed * (dif / 1000))
-    );
+    movementVec.x -= 1;
   }
   if (inputState.right) {
-    lookAtVector.applyAxisAngle(zAxis, (Math.PI * 3) / 2);
-
-    localPlayer.position.add(
-      lookAtVector.multiplyScalar(playerSpeed * (dif / 1000))
-    );
+    movementVec.x += 1;
   }
+  movementVec.normalize();
+  movementVec.applyAxisAngle(zAxis, localPlayer.yaw);
+
+  localPlayer.position.add(movementVec.multiplyScalar(playerSpeed * (dif / 1000)));
+
+  localPlayer.position.add(localPlayer.velocity.clone().multiplyScalar(dif / 1000));
+
+  if (localPlayer.position.z >= 0) 
+    localPlayer.velocity.add(grav.clone().multiplyScalar(dif / 1000));
+
+  if (localPlayer.position.z < 0) localPlayer.position.z = 0;
 
   setCameraToLocalPlayer();
 
