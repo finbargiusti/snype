03e06aee223eb1d34de3041ade1fecebdbede77a
finbargiusti <finbargiusti@gmail.com>
2019-07-15 18:29:54 +0100

    Change to better dev system

--- server/net.js
+++ server/net.js
@@ -1,37 +1,37 @@
-const http = require('http');
-const serveStatic = require('serve-static');
-const url = require('url');
+const http = require("http");
+const serveStatic = require("serve-static");
+const url = require("url");
 
-const serve = serveStatic(__dirname + '/../client', {
-    setHeaders(res) {
-        // For now. Caching is evil!
-        res.setHeader('Cache-Control', 'no-cache');
-    }
+const serve = serveStatic(__dirname + "/../client/dist", {
+  setHeaders(res) {
+    // For now. Caching is evil!
+    res.setHeader("Cache-Control", "no-cache");
+  }
 });
 
 const PORT = 20003;
 function createHTTPServer() {
-    let httpServer = http.createServer();
-    httpServer.listen(PORT);
-    
-    httpServer.on('request', (request, response) => {
-        let urlObj = url.parse(request.url, {parseQueryString: true});
-        
-        response.setHeader('Access-Control-Allow-Origin', '*');
-        
-        if (false) {
-            // Don't question me
-        } else {
-            serve(request, response, () => {
-                response.writeHead(404);
-                response.end("404");
-            });
-        }
-    });
-    
-    //(httpServer);
-    
-    console.log('HTTP server created and listening on port ' + PORT);
+  let httpServer = http.createServer();
+  httpServer.listen(PORT);
+
+  httpServer.on("request", (request, response) => {
+    let urlObj = url.parse(request.url, { parseQueryString: true });
+
+    response.setHeader("Access-Control-Allow-Origin", "*");
+
+    if (false) {
+      // Don't question me
+    } else {
+      serve(request, response, () => {
+        response.writeHead(404);
+        response.end("404");
+      });
+    }
+  });
+
+  //(httpServer);
+
+  console.log("HTTP server created and listening on port " + PORT);
 }
 
 exports.createHTTPServer = createHTTPServer;
