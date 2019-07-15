19347ba7531b1dd0fa90fb5fa7ee8f729534aff3
finbargiusti <finbargiusti@gmail.com>
2019-07-15 19:14:06 +0100

    R and D

--- client/src/ts/rendering.ts
+++ client/src/ts/rendering.ts
@@ -2,6 +2,18 @@ import * as THREE from "three";
 
 // CONFIG
 
+class Player {
+  public position: THREE.Vector3;
+
+  constructor(x: number, y: number, z: number) {
+    this.position = new THREE.Vector3(x, y, z);
+  }
+}
+
+let bob = new Player(0, 0, 0);
+
+let localPlayer = bob;
+
 const FOV = 75;
 
 let scene = new THREE.Scene();
@@ -16,12 +28,24 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+let floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10, 10, 10),
 
-camera.position.z = 5;
+  new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true })
+);
+
+let light = new THREE.PointLight(0xffffff, 100, 100000, 1);
+
+light.position.z = 1;
+
+// floor.rotation.x = (Math.PI / 2) * -1;
+
+camera.position.set(0, 0, 1);
+camera.rotateX(Math.PI / 2);
+// camera.lookAt(floor.position);
+
+scene.add(floor);
+scene.add(light);
 
 let animate = () => {
   requestAnimationFrame(animate);
